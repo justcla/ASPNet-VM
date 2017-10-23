94bc8be0a5e23031e1bd4ce5c83ed1098ae36b48
Justin Clareburt <justcla@microsoft.com>
2017-10-22 22:01:49 -0700

    Initial upload of Powershell setup script and WebDeploy MSI

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+Install-WindowsFeature -Name Web-Server
+Install-WindowsFeature -Name Web-Mgmt-Service
+
+$tempFile = [System.IO.Path]::GetTempFileName() |
+    Rename-Item -NewName { $_ -replace 'tmp$', 'exe' } -PassThru
+Invoke-WebRequest -Uri http://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi -OutFile $tempFile
+
+$logFile = [System.IO.Path]::GetTempFileName()
+
+$proc = (Start-Process $tempFile -PassThru "/quiet /install /log $logFile")
+$proc | Wait-Process
+Get-Content $logFile
